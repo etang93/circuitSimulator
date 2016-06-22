2ebb3db137b3502588a90d9008123b8f9569ba6a
etang93 <eddietang715@gmail.com>
2016-06-22 19:07:19 -0400

    Removed Dead code
    See message

--- circuit.go
+++ circuit.go
@@ -7,82 +7,92 @@ import (
     "os"
     "strconv"
     s "strings"
+    "sync"
 )
 
-func And(inputC1, inputC2, c chan int) {
+func And(inputC1, inputC2, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	input2 := <-inputC2
 	retVal := 0
 	if(input1 == 1 && input2 == 1){
 		retVal = 1
 	}
-	fmt.Println("Finished in And ", input1)
+	
 	c <- retVal
+	wg.Done()
 }
 
-func Or(inputC1, inputC2, c chan int) {
+func Or(inputC1, inputC2, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	input2 := <-inputC2
 	retVal := 0 
 	if(input1 == 1 || input2 == 1){
 		retVal = 1
 	}
-	fmt.Println("Finished in Or")
+	//fmt.Println("Finished in Or")
 	c <- retVal
+	wg.Done()
 }
 
-func Not(inputC1, c chan int) {
+func Not(inputC1, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	retVal := 0
 	if(input1 == 0){
 		retVal = 1
 	}
-	fmt.Println("Finished in Not")
+	//fmt.Println("Finished in Not")
 	c <- retVal
+	wg.Done()
 }
 
-func Nand(inputC1, inputC2, c chan int) {
+func Nand(inputC1, inputC2, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	input2 := <-inputC2
 	retVal := 0
 	if(input1 == input2){
 		retVal = 1
 	}
-	fmt.Println("Finished in Nand")
+	//fmt.Println("Finished in Nand")
 	c <- retVal
+	wg.Done()
 }
 
-func Nor(inputC1, inputC2, c chan int) {
+func Nor(inputC1, inputC2, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	input2 := <-inputC2
 	retVal := 0
 	if(input1 == 0 && input2 == 0){
 		retVal = 1
 	}
-	fmt.Println("Finished in Nor")
+	//fmt.Println("Finished in Nor")
 	c <- retVal
+	wg.Done()
 }
 
-func Xor(inputC1, inputC2, c chan int) {
+func Xor(inputC1, inputC2, c chan int, wg *sync.WaitGroup) {
 	input1 := <-inputC1
 	input2 := <-inputC2
 	retVal := 0
 	if(input1 != input2){
 		retVal = 1
 	}
-	fmt.Println("Finished in Xor ", input1)
+	//fmt.Println("Finished in Xor ", input1)
 	c <- retVal
+	wg.Done()
 }
 
-func intersection(input, output1, output2 chan int) {
+func intersection(input, output1, output2 chan int, wg *sync.WaitGroup) {
 	retVal := <- input
 	output1 <- retVal
 	output2 <- retVal
-	fmt.Println("Finished in intersection")
+	wg.Done()
+	//fmt.Println("Finished in intersection")
 }
 
-func Output(output1 chan int, outputNum int){
+func Output(output1 chan int, outputNum int, wg *sync.WaitGroup){
 	fmt.Println("The output for pipe ", outputNum, " is : ", <-output1)
+	wg.Done()
+	fmt.Println("Finished in Output")
 }
 
 func readFile(filename string) []string{
@@ -126,7 +136,8 @@ func addChannels(channels []chan int, externals int) []chan int{
 	return channels
 }
 
-func pipeline(commands []string, channels []chan int){
+func pipeline(commands []string, channels []chan int, wait *sync.WaitGroup){
+	var wg sync.WaitGroup
 	for _, lines := range commands {
 		split := s.Split(lines, " ")
 
@@ -140,7 +151,8 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err AND:", err, "split[3]: ", split[3])}
-						go And(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go And(channels[val1], channels[val2], channels[val3], &wg)
 			case "OR": 	val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err OR:", err, "split[1]: ", split[1])}
@@ -150,14 +162,16 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err OR:", err, "split[3]: ", split[3])}
-						go Or(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go Or(channels[val1], channels[val2], channels[val3], &wg)
 			case "NOT": val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err: NOT ", err, "split[1]: ", split[1])}
 						val2, err:= strconv.Atoi(split[2])
 						if err != nil {
 							println("err: NOT ", err, "split[2]: ", split[2])}
-						go Not(channels[val1], channels[val2])
+						wg.Add(1)
+						go Not(channels[val1], channels[val2], &wg)
 			case "NAND":val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err: NAND ", err, "split[1]: ", split[1])}
@@ -167,7 +181,8 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err: NAND ", err, "split[3]: ", split[3])}
-						go Nand(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go Nand(channels[val1], channels[val2], channels[val3], &wg)
 			case "NOR":val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err: NOR ", err, "split[1]: ", split[1])}
@@ -177,7 +192,8 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err: NOR ", err, "split[3]: ", split[3])}
-						go Nor(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go Nor(channels[val1], channels[val2], channels[val3], &wg)
 			case "XOR":val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err: XOR ", err, "split[1]: ", split[1])}
@@ -187,7 +203,8 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err: XOR ", err, "split[3]: ", split[3])}
-						go Xor(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go Xor(channels[val1], channels[val2], channels[val3], &wg)
 			case "INTERSECT": val1, err:= strconv.Atoi(split[1])
 						if err != nil {
 							println("err: INTERSECTION ", err, "split[1]: ", split[1])}
@@ -197,20 +214,22 @@ func pipeline(commands []string, channels []chan int){
 						val3, err:= strconv.Atoi(split[3])
 						if err != nil {
 							println("err: INTERSECTION", err, "split[3]: ", split[3])}
-						go intersection(channels[val1], channels[val2], channels[val3])
+						wg.Add(1)
+						go intersection(channels[val1], channels[val2], channels[val3], &wg)
 			case "OUTPUT": val1, err:= strconv.Atoi(split[1])
 						if err != nil {
-							println("err: OUTPUT ", err, "split[1]: ", split[1])}
-						go Output(channels[val1], val1)
-			default: fmt.Println("Error in Pipeline")
+							fmt.Println("err: OUTPUT ", err, "split[1]: ", split[1])
+						}
+						wg.Add(1)
+						go Output(channels[val1], val1, &wg)
+						
+			default: fmt.Println("Error in Pipeline", split[0])
 		}
-		
-		/*if s, err := strconv.Atoi(split[1]); err == nil {
-			fmt.Printf("%T, %v\n", s, s)
-		}*/
-
 	}
+	wg.Wait()
+	wait.Done()
 }
+
 
 func main() {
 	var flipFlopState int
@@ -221,8 +240,8 @@ func main() {
 	var channelNum int
 	var numExternals int
 	var filename string
-	//filename := "C:/Go/workspace/src/circuit/bin/blah.txt"
-	
+	var wg sync.WaitGroup
+
 	fmt.Println("What is the filename with the circuit description?")
 	fmt.Scanf("%s\n", &filename)
 
@@ -265,9 +284,9 @@ func main() {
 		fmt.Println("How many clock pulses do you want in the simulation?")
 		fmt.Scanf("%d\n", &clockPulsesTot)
 	}
-	
-	go pipeline(commands, ch)
-	
-	fmt.Println("ch[15]: ", <-ch[15])
-
+	wg.Add(1)
+	go pipeline(commands, ch, &wg)
+	wg.Wait()
+	//fmt.Println("ch[15]: ", <-ch[15])
+	fmt.Println("Thank you for using this system!")
 }
